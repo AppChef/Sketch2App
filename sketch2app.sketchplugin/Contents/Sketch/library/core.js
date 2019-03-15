8ccb19c4e99db812fddcdbd2c6186e7046f2553b
guofoo <guofoo@gmail.com>
2019-03-14 18:44:26 -0700

    update names for AppChef

--- sketch2app.sketchplugin/Contents/Sketch/library/core.js
+++ sketch2app.sketchplugin/Contents/Sketch/library/core.js
@@ -1,9 +1,9 @@
 @import 'library/common.js'
 @import 'library/analytics.js'
 
-var l4c = {
+const appchef = {
   "defs": {
-    "pluginVersion": "Version 0.4.5",
+    "pluginVersion": "Version 0.5.0",
     "apiBase": "https://cloud.appchef.io/",
     "apiSignin": "login",
     "apiUpload": "sketch",
@@ -21,7 +21,7 @@ var l4c = {
   getSavedValueFromKey: function(key){
     return [[NSUserDefaults standardUserDefaults] objectForKey:key]
   },
-  
+
   saveValueForKey: function(value, key){
     [[NSUserDefaults standardUserDefaults] setObject:value forKey:key]
     [[NSUserDefaults standardUserDefaults] synchronize]
@@ -40,11 +40,11 @@ var l4c = {
   },
 
   tokenValid: function(context) {
-    var idToken = l4c.getSavedValueFromKey("idToken")
+    var idToken = appchef.getSavedValueFromKey("idToken")
     log("get saved token " + idToken)
     if (!idToken) return false
     var token = "Bearer " + idToken
-    var url = [NSURL URLWithString:l4c.defs.apiBase + l4c.defs.apiCheck]
+    var url = [NSURL URLWithString:appchef.defs.apiBase + appchef.defs.apiCheck]
     var request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60]
     [request setHTTPMethod:"GET"]
     [request setValue:"sketch" forHTTPHeaderField:"User-Agent"]
@@ -59,7 +59,7 @@ var l4c = {
       if (res.message == "success") return true
       else return false
     } else {
-      l4c.showAlert("😞 User session expired. Please login again.", context)
+      appchef.showAlert("😞 User session expired. Please login again.", context)
       return false
     }
   },
@@ -100,7 +100,7 @@ var l4c = {
   },
 
   loginWithEmailAndPassword: function(context, email, password){
-    var url = [NSURL URLWithString:l4c.defs.apiBase + l4c.defs.apiSignin]
+    var url = [NSURL URLWithString:appchef.defs.apiBase + appchef.defs.apiSignin]
     var request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60]
     [request setHTTPMethod:"POST"]
     [request setValue:"sketch" forHTTPHeaderField:"User-Agent"]
@@ -115,8 +115,8 @@ var l4c = {
     var data = [NSURLConnection sendSynchronousRequest:request returningResponse:response error:error]
     if (error.value() == nil && data != nil){
       var res = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil]
-      l4c.saveValueForKey(res.id_token, "idToken")
-      l4c.saveValueForKey(res.user_id, "userId")
+      appchef.saveValueForKey(res.id_token, "idToken")
+      appchef.saveValueForKey(res.user_id, "userId")
       ga.send(context, {ec: 'login', ea: 'login', el: email, ev: 1, uid: res.user_id})
       return true
     } else {
@@ -126,55 +126,55 @@ var l4c = {
   },
 
   loginToExport: function(context) {
-    var response = l4c.showLoginDialog(context)
+    var response = appchef.showLoginDialog(context)
     if (response[0] == 1000) {
-      var response = l4c.loginWithEmailAndPassword(context, response[1], response[2])
-      l4c.showMessage("Login Success. Generating Schema ...", context)
+      var response = appchef.loginWithEmailAndPassword(context, response[1], response[2])
+      appchef.showMessage("Login Success. Generating Schema ...", context)
       if (response == 1) {
-        l4c.exportSchema(context)
+        appchef.exportSchema(context)
       } else {
-        l4c.showAlert("Login failed. Please check your credentials and try again", context)
+        appchef.showAlert("Login failed. Please check your credentials and try again", context)
       }
     }
   },
 
   exportWithoutLogin: function(context) {
-    var response = l4c.showUploadDialog(context)
-    if (response == 1000) l4c.exportSchema(context)
+    var response = appchef.showUploadDialog(context)
+    if (response == 1000) appchef.exportSchema(context)
   },
 
   exportSchema: function(context) {
-    var uid = l4c.getSavedValueFromKey("userId")
+    var uid = appchef.getSavedValueFromKey("userId")
     var document = context.document
     var baseDir = helpers.getCurrentDirectory(document)
     var filename = document.fileURL().lastPathComponent()
     var logging = filename + ", "
-    helpers.removeFileOrFolder(baseDir + "/" + l4c.defs.localFolder)
-    helpers.removeFileOrFolder(baseDir + "/" + l4c.defs.localFolder + "-schema.zip")
-    helpers.removeFileOrFolder(baseDir + "/" + l4c.defs.localFolder + "-assets.zip")
-    helpers.createFolderAtPath(baseDir + "/" + l4c.defs.localFolder)
-    l4c.logger(context, "debug", logging + "create local appchef folder " + l4c.defs.localFolder)
+    helpers.removeFileOrFolder(baseDir + "/" + appchef.defs.localFolder)
+    helpers.removeFileOrFolder(baseDir + "/" + appchef.defs.localFolder + "-schema.zip")
+    helpers.removeFileOrFolder(baseDir + "/" + appchef.defs.localFolder + "-assets.zip")
+    helpers.createFolderAtPath(baseDir + "/" + appchef.defs.localFolder)
+    appchef.logger(context, "debug", logging + "create local appchef folder " + appchef.defs.localFolder)
     try {
-      helpers.exec(document, "/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool dump \"" + filename + "\" > " + l4c.defs.localFolder + "/raw.json")
-      l4c.logger(context, "debug", logging + "generated sketch json schema")      
+      helpers.exec(document, "/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool dump \"" + filename + "\" > " + appchef.defs.localFolder + "/raw.json")
+      appchef.logger(context, "debug", logging + "generated sketch json schema")
     } catch (err) {
       ga.send(context, {exd: 'SketchToolDumpError', uid: uid, el: uid, ev: 1})
-      l4c.showAlert("Use sketchtool failed. Please install Homebrew and try install sketchtool again.", context)
-      l4c.logger(context, "error", logging + "fail to call sketchtool dump. Error is " + JSON.stringify(err))  
+      appchef.showAlert("Use sketchtool failed. Please install Homebrew and try install sketchtool again.", context)
+      appchef.logger(context, "error", logging + "fail to call sketchtool dump. Error is " + JSON.stringify(err))
     }
     try {
-      helpers.exec(document, "zip -r -X " + l4c.defs.localFolder + "-schema.zip " + l4c.defs.localFolder)
+      helpers.exec(document, "zip -r -X " + appchef.defs.localFolder + "-schema.zip " + appchef.defs.localFolder)
     } catch (err) {
       ga.send(context, {exd: 'CompressError', uid: uid, el: uid, ev: 1})
-      l4c.showAlert("Compress schema folder " + l4c.defs.localFolder + " failed. Please contact us to fix the problem.", context)
-      l4c.logger(context, "error", logging + "fail to compress appchef schema folder. Error is " + JSON.stringify(err))  
+      appchef.showAlert("Compress schema folder " + appchef.defs.localFolder + " failed. Please contact us to fix the problem.", context)
+      appchef.logger(context, "error", logging + "fail to compress appchef schema folder. Error is " + JSON.stringify(err))
     }
-    l4c.upload(baseDir + "/" + l4c.defs.localFolder + "-schema.zip", filename, 'schema', context)
+    appchef.upload(baseDir + "/" + appchef.defs.localFolder + "-schema.zip", filename, 'schema', context)
   },
 
   exportAssets: function(context) {
-    helpers.createFolderAtPath(baseDir + "/" + l4c.defs.localFolder + "/images")
-    var uid = l4c.getSavedValueFromKey("userId")
+    helpers.createFolderAtPath(baseDir + "/" + appchef.defs.localFolder + "/images")
+    var uid = appchef.getSavedValueFromKey("userId")
     var document = context.document
     var selection = document.allExportableLayers()
     var baseDir = helpers.getCurrentDirectory(document)
@@ -182,16 +182,16 @@ var l4c = {
     var logging = filename + ", "
     for (var i = 0; i < [selection count]; i++) {
       var layer = selection[i]
-      l4c.processSlice(layer, document)
+      appchef.processSlice(layer, document)
     }
-    l4c.logger(context, "debug", logging + "exported all assets from sketch") 
+    appchef.logger(context, "debug", logging + "exported all assets from sketch")
     try {
-      helpers.exec(document, "zip -r -X " + l4c.defs.localFolder + "-assets.zip " + l4c.defs.localFolder + "/images")
+      helpers.exec(document, "zip -r -X " + appchef.defs.localFolder + "-assets.zip " + appchef.defs.localFolder + "/images")
     } catch (err) {
-      l4c.showAlert("Compress assets directory " + l4c.defs.localFolder + "/images faild. Please contact us to fix the problem.", context)
-      l4c.logger(context, "error", logging + "fail to compress appchef assets folder. Error is " + JSON.stringify(err))
+      appchef.showAlert("Compress assets directory " + appchef.defs.localFolder + "/images faild. Please contact us to fix the problem.", context)
+      appchef.logger(context, "error", logging + "fail to compress appchef assets folder. Error is " + JSON.stringify(err))
     }
-    l4c.upload(baseDir + "/" + l4c.defs.localFolder + "-assets.zip", filename, 'assets', context)
+    appchef.upload(baseDir + "/" + appchef.defs.localFolder + "-assets.zip", filename, 'assets', context)
   },
 
   processSlice: function(slice, document) {
@@ -200,11 +200,11 @@ var l4c = {
     var sliceName = ([slice name]).replace(/[^A-Za-z0-9._-]/g, '-')
     var baseDir = helpers.getCurrentDirectory(document)
 
-    for (var i = 0; i < l4c.defs.factors.length; i++) {
-      var scale = l4c.defs.factors[i].scale
-      var suffix = l4c.defs.factors[i].suffix
-      var version = l4c.makeSliceAndResizeWithFactor(slice, scale)
-      var fileName = baseDir + "/" + l4c.defs.localFolder + "/images/" + sliceName + "-" + objectID + suffix + ".png"
+    for (var i = 0; i < appchef.defs.factors.length; i++) {
+      var scale = appchef.defs.factors[i].scale
+      var suffix = appchef.defs.factors[i].suffix
+      var version = appchef.makeSliceAndResizeWithFactor(slice, scale)
+      var fileName = baseDir + "/" + appchef.defs.localFolder + "/images/" + sliceName + "-" + objectID + suffix + ".png"
       [document saveArtboardOrSlice: version toFile: fileName]
       log("Saved " + fileName)
     }
@@ -239,12 +239,12 @@ var l4c = {
   },
 
   upload: function(filePath, project, type, context) {
-    var uid = l4c.getSavedValueFromKey("userId")
-    var token = l4c.getSavedValueFromKey("idToken")
+    var uid = appchef.getSavedValueFromKey("userId")
+    var token = appchef.getSavedValueFromKey("idToken")
     var task = NSTask.alloc().init()
     var logging = project + ", "
     task.setLaunchPath("/usr/bin/curl")
-    var args = NSArray.arrayWithArray(["-X", "POST", "-H", "Authorization: Bearer " + token, "-F", "project=" + project, "-F", "type=" + type, "-F", "assets=@" + filePath, l4c.defs.apiBase + l4c.defs.apiUpload])
+    var args = NSArray.arrayWithArray(["-X", "POST", "-H", "Authorization: Bearer " + token, "-F", "project=" + project, "-F", "type=" + type, "-F", "assets=@" + filePath, appchef.defs.apiBase + appchef.defs.apiUpload])
     task.setArguments(args)
     var outputPipe = [NSPipe pipe]
     [task setStandardOutput:outputPipe]
@@ -255,24 +255,24 @@ var l4c = {
     log(outputString)
     if(outputArray["message"] != "success"){
       ga.send(context, {exd: 'UploadError', exf: 1, uid: uid, el: uid, ev: 1})
-      l4c.logger(context, "error", logging + "fail to upload " + type + ". Error is " + outputArray["message"])
-      l4c.showAlert(outputArray["message"], context)
+      appchef.logger(context, "error", logging + "fail to upload " + type + ". Error is " + outputArray["message"])
+      appchef.showAlert(outputArray["message"], context)
     } else {
-      l4c.logger(context, "debug", logging + "success upload " + type)
+      appchef.logger(context, "debug", logging + "success upload " + type)
       if (type === 'schema') {
-        l4c.showMessage("Sketch schema upload success. Now uploading image assets, upload time depends on the size of assets. Please wait and don't close sketch ...", context)
-        l4c.exportAssets(context)
+        appchef.showMessage("Sketch schema upload success. Now uploading image assets, upload time depends on the size of assets. Please wait and don't close sketch ...", context)
+        appchef.exportAssets(context)
       } else {
         ga.send(context, {ec: 'upload', ea: 'upload', uid: uid, el: uid, ev: 1})
-        l4c.showAlert("👍 Upload success. Open the Appchef app on your phone to see the project.", context)
+        appchef.showAlert("👍 Upload success. Open the Appchef app on your phone to see the project.", context)
       }
     }
   },
 
   logger: function(context, level, message){
     log('send message ' + level + ' ' + message);
-    var url = [NSURL URLWithString:l4c.defs.apiBase + l4c.defs.apiLog]
-    var token = "Bearer "+l4c.getSavedValueFromKey("idToken")
+    var url = [NSURL URLWithString:appchef.defs.apiBase + appchef.defs.apiLog]
+    var token = "Bearer "+appchef.getSavedValueFromKey("idToken")
     var request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60]
     [request setHTTPMethod:"POST"]
     [request setValue:"sketch" forHTTPHeaderField:"User-Agent"]
@@ -286,26 +286,26 @@ var l4c = {
   },
 
   installSketchtool: function(context) {
-    var uid = l4c.getSavedValueFromKey("userId")
+    var uid = appchef.getSavedValueFromKey("userId")
     try {
       var res = helpers.exec(context.document, "/Applications/Sketch.app/Contents/Resources/sketchtool/install.sh")
       ga.send(context, {ec: 'install-sketchtool', ea: 'install-sketchtool', uid: uid, el: uid, ev: 1})
-      l4c.showAlert(res, context)
+      appchef.showAlert(res, context)
     } catch (error) {
       ga.send(context, {exd: 'InstallSketchToolError', exf: 1, uid: uid, el: uid, ev: 1})
       log("receive error " + error)
-      l4c.logger(context, "error", "Fail to install sketchtool. Error is " + JSON.stringify(error))
-      l4c.showAlert("Install sketchtool failed. Please install Homebrew and try again.", context)
+      appchef.logger(context, "error", "Fail to install sketchtool. Error is " + JSON.stringify(error))
+      appchef.showAlert("Install sketchtool failed. Please install Homebrew and try again.", context)
     }
   },
 
   logoutFromSketch: function(context){
-    var uid = l4c.getSavedValueFromKey("userId")
+    var uid = appchef.getSavedValueFromKey("userId")
     ga.send(context, {ec: 'logout', ea: 'logout', uid: uid, el: uid, ev: 1})
-    l4c.saveValueForKey(nil, "idToken")
-    l4c.saveValueForKey(nil, "userId")
-    l4c.saveValueForKey(nil, "currentVersion")
-    l4c.showMessage("Logout success", context)
+    appchef.saveValueForKey(nil, "idToken")
+    appchef.saveValueForKey(nil, "userId")
+    appchef.saveValueForKey(nil, "currentVersion")
+    appchef.showMessage("Logout success", context)
   },
 
 }
